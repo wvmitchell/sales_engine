7be63ef206adcc563f0ec89b6e7ac0eca5af0ae1
Will Mitchell <wvmitchell@gmail.com>
2013-09-27 15:14:44 -0600

    Added Item relationship methods invoice_item and merchant, and associated tests

--- lib/item.rb
+++ lib/item.rb
@@ -1,5 +1,17 @@
 require_relative 'base_unit'
 
 class Item < BaseUnit
+
+  def invoice_item
+    sales_engine_reference.invoice_item_repository.collection_array.find do |ii|
+      ii.item_id == id 
+    end
+  end
+
+  def merchant
+    sales_engine_reference.merchant_repository.collection_array.find do |mer|
+      mer.id == merchant_id
+    end
+  end
   
 end
--- test/item_test.rb
+++ test/item_test.rb
@@ -5,14 +5,17 @@ require_relative '../lib/item'
 
 class ItemTest < MiniTest::Unit::TestCase
   
-  attr_reader :item
+  attr_reader :item, :se
 
   def setup
-    data = {id: 1,
+    @se = SalesEngine.new
+    se.invoice_repository
+    data = {id: '539',
 	    name: "Rolen",
 	    description: "Awesome",
-	    unit_price: 100,
-	    merchant_id: 10
+	    unit_price: '100',
+	    merchant_id: '10',
+      sales_engine_reference: se
             #created_at: "date"
             #updated_at: "data
            }
@@ -24,7 +27,7 @@ class ItemTest < MiniTest::Unit::TestCase
   end
 
   def test_id_is_set
-    assert_equal 1, item.id
+    assert_equal '539', item.id
   end
 
   def test_name_is_set
@@ -36,10 +39,34 @@ class ItemTest < MiniTest::Unit::TestCase
   end
 
   def test_unit_price_is_set
-    assert_equal 100, item.unit_price
+    assert_equal '100', item.unit_price
   end
 
   def test_merchant_id_is_set
-    assert_equal 10, item.merchant_id
+    assert_equal '10', item.merchant_id
+  end
+
+  def test_invoice_item_method_exists
+    assert item.methods.include?(:invoice_item)
+  end
+
+  def test_invoice_item_method_returns_invoice_item_type
+    assert_kind_of InvoiceItem, item.invoice_item
+  end
+
+  def test_invoice_item_method_returns_invoice_item_associated_with_item
+    assert_equal item.id, item.invoice_item.item_id
+  end
+
+  def test_merchant_method_exists
+    assert item.methods.include?(:merchant), "No merchant method defined"
+  end
+
+  def test_merchant_method_returns_a_merchant_type
+    assert_kind_of Merchant, item.merchant 
+  end
+
+  def test_merchant_method_returns_merchant_associated_with_item
+    assert_equal item.merchant_id, item.merchant.id
   end
 end  
