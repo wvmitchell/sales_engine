78988cb62402ba9545c85271ef208e43e37b3a50
Will Mitchell <wvmitchell@gmail.com>
2013-09-27 14:53:33 -0600

    Added InvoiceItem relationship methods invoice and item, with associated tests

--- lib/invoice_item.rb
+++ lib/invoice_item.rb
@@ -1,4 +1,17 @@
 require_relative 'base_unit'
+require_relative 'invoice'
 
 class InvoiceItem < BaseUnit
+
+  def invoice
+    sales_engine_reference.invoice_repository.collection_array.find do |inv|
+      inv.id == invoice_id
+    end
+  end
+
+  def item
+    sales_engine_reference.item_repository.collection_array.find do |it|
+      it.id == item_id
+    end
+  end
 end
--- test/invoice_item_test.rb
+++ test/invoice_item_test.rb
@@ -5,14 +5,17 @@ require './lib/invoice_item'
 
 class InvoiceItemTest < MiniTest::Unit::TestCase
   
-  attr_reader :ii
+  attr_reader :ii, :se
 
   def setup
-    data = {id: 1,
-	    item_id: 12,
-	    invoice_id: 13,
-	    quantity: 44,
-	    unit_price: 100
+    @se = SalesEngine.new
+    se.invoice_repository
+    data = {id: '1',
+	    item_id: '12',
+	    invoice_id: '13',
+	    quantity: '44',
+	    unit_price: '100',
+      sales_engine_reference: se
     }
     @ii = InvoiceItem.new(data)
   end
@@ -22,22 +25,46 @@ class InvoiceItemTest < MiniTest::Unit::TestCase
   end
   
   def test_id_is_set
-    assert_equal 1, ii.id
+    assert_equal '1', ii.id
   end
 
   def test_item_id_is_set
-    assert_equal 12, ii.item_id
+    assert_equal '12', ii.item_id
   end
 
   def test_invoice_id_is_set
-    assert_equal 13, ii.invoice_id
+    assert_equal '13', ii.invoice_id
   end
 
   def test_quantity_is_set
-    assert_equal 44, ii.quantity
+    assert_equal '44', ii.quantity
   end
 
   def test_unit_price_is_set
-    assert_equal 100, ii.unit_price
+    assert_equal '100', ii.unit_price
+  end
+
+  def test_invoice_method_is_defined
+    assert ii.methods.include?(:invoice), "No invoice method defined"
+  end
+
+  def test_invoice_method_returns_an_invoice_type
+    assert_kind_of Invoice, ii.invoice
+  end
+
+  def test_invoce_method_returns_invoice_associated_with_invoice_item
+    assert_equal ii.invoice_id, ii.invoice.id 
+  end
+
+  def test_items_method_exists
+    assert ii.methods.include?(:item)
+  end
+
+  def test_item_method_returns_an_item_type
+    assert_kind_of Item, ii.item
+  end
+
+  def test_item_method_returns_item_associated_with_invoice_item
+    assert_equal ii.item_id, ii.item.id
   end
 end
