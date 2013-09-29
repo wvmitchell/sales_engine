471cf5fa3c00ab95b66f0548ade700cb163fca6e
Rolen Le <rolentle@gmail.com>
2013-09-29 13:52:29 -0600

    saving changes before pulling master.

--- lib/invoice_item.rb
+++ lib/invoice_item.rb
@@ -4,24 +4,14 @@ require_relative 'invoice'
 class InvoiceItem < BaseUnit
 
   def invoice
-<<<<<<< HEAD
-    sales_engine_reference.invoice_repository.collection_array.find do |invoice|
-      invoice.id == invoice_id 
-=======
     sales_engine_reference.invoice_repository.collection_array.find do |inv|
       inv.id == invoice_id
->>>>>>> 7be63ef206adcc563f0ec89b6e7ac0eca5af0ae1
     end
   end
 
   def item
-<<<<<<< HEAD
-    sales_engine_reference.item_repository.collection_array.find do |item|
-      item.id == item_id 
-=======
     sales_engine_reference.item_repository.collection_array.find do |it|
       it.id == item_id
->>>>>>> 7be63ef206adcc563f0ec89b6e7ac0eca5af0ae1
     end
   end
 end
